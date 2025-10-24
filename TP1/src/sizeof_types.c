a4a62f3b3f41b16a07f2dab33adb8fa409fccff7
mattdnra <matteo8.tablette@gmail.com>
2025-10-24 10:20:42 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -4,14 +4,24 @@ int main(void) {
     printf("Tailles des types de base en octets :\n\n");
 
     printf("char              : %zu octet(s)\n", sizeof(char));
-    printf("short             : %zu octet(s)\n", sizeof(short));
-    printf("int               : %zu octet(s)\n", sizeof(int));
-    printf("long int          : %zu octet(s)\n", sizeof(long int));
-    printf("long long int     : %zu octet(s)\n", sizeof(long long int));
-    printf("float             : %zu octet(s)\n", sizeof(float));
+    printf("unsigned char     : %zu octet(s)\n", sizeof(unsigned char));
+    printf("signed char       : %zu octet(s)\n", sizeof(signed char));
+
+    printf("\nshort             : %zu octet(s)\n", sizeof(short));
+    printf("unsigned short    : %zu octet(s)\n", sizeof(unsigned short));
+
+    printf("\nint               : %zu octet(s)\n", sizeof(int));
+    printf("unsigned int      : %zu octet(s)\n", sizeof(unsigned int));
+
+    printf("\nlong int          : %zu octet(s)\n", sizeof(long int));
+    printf("unsigned long int : %zu octet(s)\n", sizeof(unsigned long int));
+
+    printf("\nlong long int          : %zu octet(s)\n", sizeof(long long int));
+    printf("unsigned long long int : %zu octet(s)\n", sizeof(unsigned long long int));
+
+    printf("\nfloat             : %zu octet(s)\n", sizeof(float));
     printf("double            : %zu octet(s)\n", sizeof(double));
     printf("long double       : %zu octet(s)\n", sizeof(long double));
 
     return 0;
 }
-
