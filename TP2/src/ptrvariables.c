e65f19aac9f81c2c8d1cc5d2c8acd203b6797a14
mattdnra <matteo8.tablette@gmail.com>
2025-12-02 10:43:11 +0100

    Update ptrvariables.c

--- TP2/src/ptrvariables.c
+++ TP2/src/ptrvariables.c
@@ -2,9 +2,9 @@
 
 int main() {
     int i = 0x12345678;
-    int *p = &i;
-    printf("%p %x\n", (void*)p, *p);
-    *p -= 1;
-    printf("%p %x\n", (void*)p, *p);
+    int *pi = &i;
+    printf("Adresse de i : %p, Valeur de i : %x\n", (void*)pi, *pi);
+    *pi -= 1;
+    printf("Adresse de i : %p, Valeur de i : %x\n", (void*)pi, *pi);
     return 0;
 }
